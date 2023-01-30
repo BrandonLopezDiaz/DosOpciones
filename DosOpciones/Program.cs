e2994ec2408fcf9788e784bfee649efe582aefff
Brandon <brandon.lopes.diaz@gmail.com>
2023-01-30 17:26:42 -0500

    Agregar archivos de proyecto.

---
+++ DosOpciones/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DosOpciones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Reps;
+                do
+                {
+                    Console.Clear();
+                    operaciones OPS = new operaciones();
+                    Console.WriteLine("Seleccione la operación deseada");
+                    Console.WriteLine("Escriba 1 para sumar\r\n" +
+                        "Escriba 2 para restar");
+                    int SW = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    switch (SW)
+                    {
+                        case 1:
+                            OPS.Suma();
+                            break;
+                        case 2:
+                            OPS.Resta();
+                            break;
+                        default: Console.WriteLine("La opción " + SW + " no es válida"); break;
+                    }
+                    Console.WriteLine("Si desea continuar entonces presione y");
+                    Reps = Console.ReadLine();
+                } while (Reps !="Y" || Reps !="y");  
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error:" + ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ DosOpciones/operaciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DosOpciones
+{
+    public class operaciones
+    {
+        public void Suma()
+        {
+            Console.WriteLine("Escogió la suma");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Ingrese por favor los 5 números");
+            Console.WriteLine("Primer número: "); Num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo número: "); Num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tercero número: "); Num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cuarto número: "); Num4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quinto número: "); Num5 = int.Parse(Console.ReadLine());
+            Num6 = Num1 + Num2 + Num3 + Num4 + Num5;
+            Console.Clear();
+            Console.WriteLine("Números a sumar: "+Num1+"+"+Num2+"+"+ Num3+"+"+ Num4+"+"+ Num5+" El resultado es: "+Num6);
+            Console.ReadKey();
+        }
+        public void Resta()
+        {
+            Console.WriteLine("Escogió la resta");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Ingrese por favor los 5 números");
+            Console.WriteLine("Primer número: "); Num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo número: "); Num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tercero número: "); Num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cuarto número: "); Num4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quinto número: "); Num5 = int.Parse(Console.ReadLine());
+            Num6 = Num1 - Num2 - Num3 - Num4 - Num5;
+            Console.Clear();
+            Console.WriteLine("Números a restar: " + Num1 + "-" + Num2 + "-" + Num3 + "-" + Num4 + "-" + Num5 + " El resultado es: " + Num6);
+            Console.ReadKey();
+        }
+        public int Num1, Num2, Num3, Num4, Num5, Num6;
+
+    }
+}
